8efd74a09d0f7940edbfecc16677a9ac40a723b6
Admin <reteamboss@gmail.com>
2024-09-23 21:23:01 +0300

    2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,19 @@
 fun main() {
-    println("Hello World!")
+
+    val names: Map<Int,String> = mapOf(
+        1 to "Tom",
+        2 to "Redik",
+        3 to "Helen",
+        4 to "Peter",
+        5 to "Cody",
+        6 to "Tom",
+        7 to "Redik",
+        8 to "Helen",
+        9 to "Peter",
+        10 to "Cody",
+        11 to "Sara",
+        12 to "Klara"
+    )
+
+    println(names.filter { it.value.endsWith('a') })
 }
